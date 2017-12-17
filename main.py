e69e3c7160599fb35e68f4f20b658e816442e02d
Tomasz Kostur <tomasz.kostur@gmail.com>
2017-12-17 16:32:47 +0100

    Start creating calculator

--- main.py
+++ main.py
@@ -3,18 +3,56 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.textinput import TextInput
 
 class Scroller(BoxLayout):
 
     def add_member(self):
-        self.ids.members.add_widget(Button(size_hint=(1, None), height=100))
+        self.ids.products.add_widget(Button(size_hint=(1, None), height=100))
+
+
+class PersonView(BoxLayout):
+    pass
+
+
+class PersonTitleBar(BoxLayout):
+    pass
+
+
+class ProductView(BoxLayout):
+
+    def add_person(self):
+        self.ids.people.add_widget(PersonView())
+
+
+class MainView(BoxLayout):
+    pass
 
 
 class LetsSettleApp(App):
     def build(self):
-        self.scroller = Scroller()
-        return self.scroller
+        self.main_view = MainView()
+        return self.main_view
 
+
+class Calculator():
+    def __init__(self):
+        self.data_dict = {}
+
+    def add_product(self, product_name):
+        self.data_dict[product_mane] = {}
+
+    def add_person(self, product_name, name, cost):
+        self.data_dict[product_name][name] = cost
+
+    def calculate_single_product(self, product_name):
+        product_dict = self.data_dict[product_name]
+        product_cost = sum([cost for cost in product_dict.values()])
+        
 
 if __name__ == '__main__':
     LetsSettleApp().run()
