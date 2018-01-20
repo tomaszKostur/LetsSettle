fadae8b57f742bad8a79d207905e6b431b3391a8
Tomasz Kostur <tomasz.kostur@gmail.com>
2018-01-20 14:43:13 +0100

    First version of Calculator done!

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.textinput import TextInput
+from copy import deepcopy
 
 class Scroller(BoxLayout):
 
@@ -40,17 +41,53 @@ class LetsSettleApp(App):
 
 
 class Calculator():
+    """
+    Calculator that makes calculation of who ows who money.
+    Structures conventions:
+        data_dict = {<str:product_name>: {<str:person>: <int:paid>,
+                                          ...}
+                     ...}
+
+        debit_dict = {<str:person>: {<str:mate_that_person_ows>: <int:ammount>,
+                                     ...}
+                      ...}
+
+        great_debit_dict = {<str:product>: debit_dict, ...}
+    """
     def __init__(self):
-        self.data_dict = {}
-        self.debtor_dict = {}
+        """
+        Declares class variables.
+        Dict's structure descriptions are in class docstring.
+        """
+        self.data_dict = {}  # structure of data_dict.
+        self.great_debit_dict = {}  # structure of great_debit_dict.
+        self.total_debit_dict = {}  # structure of debit_dict.
 
     def add_product(self, product_name):
+        """
+        Add product to self.data_dict.
+
+        product_name: string
+        """
         self.data_dict[product_name] = {}
 
-    def add_person(self, product_name, name, cost):
-        self.data_dict[product_name][name] = cost
+    def add_person(self, product_name, name, paid):
+        """
+        Adds person to self.product_name[name]: paid.
+        product_name: string, name of existing products
+        name: string, person name
+        paid: int, person cost
+        """
+        self.data_dict[product_name][name] = paid
 
     def calculate_single_product(self, product_name):
+        """
+        Calculates debits to each other for product.
+        Requires self.data_dict[product_name] filled with data.
+            The data is dictionary with people and ammount of theirs individual
+            costs.
+        product_name: string, name of the product.
+        """
         product_dict = self.data_dict[product_name]
         product_cost = sum([cost for cost in product_dict.values()])
         people_num = len(product_dict)
@@ -70,15 +107,61 @@ class Calculator():
                 debit_dict[person][mate] = cost_frac[mate] * person_cost
 
         # simplify debit_dict
+        self._simplify_debit_dict(debit_dict)
+
+        # save result in class variable
+        self.great_debit_dict[product_name] = debit_dict
+
+    @staticmethod
+    def _simplify_debit_dict(debit_dict):
+        """
+        Simplify debit_dict structure.
+            For example: if 'A' ows 'B' 5 and 'B' ows 'A' 4,
+                then 'A' ows 'B' 1
+        debit_dict: dict, see structure in class docstring.
+        returns: None, debit_dict values are changed.
+        """
         for person in debit_dict:
             for mate in debit_dict[person]:
-                if person is mate: continue
-                elif debit_dict[person][mate] > debit_dict[mate][person]:
+                if debit_dict[person][mate] >= debit_dict[mate][person]:
                     debit_dict[person][mate] -= debit_dict[mate][person]
                     debit_dict[mate][person] = 0
-
         return debit_dict
 
+    def summary(self):
+        """
+        Get all debit_dict's from self.great_debit_dict's products.
+        Merge them into one debit_dict structure.
+        Save into self.total_debit_dict
+        """
+        summary_debit_dict = {}
+        for product in self.great_debit_dict:
+            for person in self.great_debit_dict[product]:
+                if person not in summary_debit_dict:
+                    summary_debit_dict[person] = \
+                        deepcopy(self.great_debit_dict[product][person])
+                else:
+                    for mate in self.great_debit_dict[product][person]:
+                        if mate not in summary_debit_dict[person]:
+                            summary_debit_dict[person][mate] = \
+                                deepcopy(self.great_debit_dict
+                                         [product][person][mate])
+                        else:
+                            summary_debit_dict[person][mate] += \
+                                self.great_debit_dict[product][person][mate]
+
+        self._simplify_debit_dict(summary_debit_dict)
+        self.total_debit_dict = summary_debit_dict
+
+    def calculate_all(self):
+        """
+        Calculates debits for all people and all producs.
+        returns: debit_dict, dict for summary of all debits.
+        """
+        for product in self.great_debit_dict:
+            self.calculate_single_product(product)
+        self.summary()
+        return self.total_debit_dict
 
 
 if __name__ == '__main__':
--- test_lets_settle.py
+++ test_lets_settle.py
@@ -6,16 +6,42 @@ import pytest
 
 class TestCalculator:
 
-#    @pytest.fixture(scope='class')
-#    def calc():
-#        return Calculator()
+    @pytest.fixture(scope='class')
+    def calc(self):
+        return Calculator()
 
-    def test_calculator(calc):
-        calc = Calculator()
+    def test_calculator(self, calc):
         calc.add_product('mlekoihuj')
         calc.add_person('mlekoihuj', 'Tomek', 20)
         calc.add_person('mlekoihuj', 'Jacek', 10)
         calc.add_person('mlekoihuj', 'Ola', 0)
 
-        result = calc.calculate_single_product('mlekoihuj')
+        calc.calculate_single_product('mlekoihuj')
+        print("\nResults:")
+        pprint(calc.great_debit_dict['mlekoihuj'])
+
+    def test_great_debit_dict(self, calc):
+        calc.add_product('ciasteczka')
+        calc.add_person('ciasteczka', 'Tomek', 0)
+        calc.add_person('ciasteczka', 'Jacek', 10)
+        calc.add_person('ciasteczka', 'Ola', 30)
+
+        result = calc.calculate_single_product('ciasteczka')
+        print("\nResults:")
+        pprint(calc.great_debit_dict)
+
+    def test_summary(self, calc):
+        results = calc.summary()
+        print("\nResults:")
+        pprint(calc.total_debit_dict)
+
+    def test_calculate_all(self, calc):
+
+        calc.add_product('ciasteczka')
+        calc.add_person('ciasteczka', 'Tomek', 30)
+        calc.add_person('ciasteczka', 'Jacek', 10)
+        calc.add_person('ciasteczka', 'Ola', 30)
+
+        result = calc.calculate_all()
+        print("\nResults:")
         pprint(result)
